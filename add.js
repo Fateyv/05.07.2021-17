647190ff7fc2b80e369d4bda147f80ed608970cf
Artur <79041911+Fateyv@users.noreply.github.com>
2021-07-06 20:45:57 +0300

    added all

---
+++ add.js
@@ -0,0 +1,26 @@
+const listPhotos = document.getElementById('list');
+let photos = [];
+
+function getPhotos() {
+    fetch('https://jsonplaceholder.typicode.com/photos?albumId=1')
+    .then((resp) => {resp.json()
+                    .then((data) => photos = data)
+                    .then(renderPhotos);
+    });
+}
+
+function renderPhotos() {
+    photos.forEach((item) => renderPhoto(item));
+}
+
+function renderPhoto(photo) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+
+    img.src = photo.thumbnailUrl;
+    li.appendChild(img);
+    listPhotos.appendChild(li);
+}
+
+getPhotos();
+
